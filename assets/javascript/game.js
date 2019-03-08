e52aee21f19522d266c4fae83be5b45d73574a6c
HowardKuo <howardkuo91@gmail.com>
2019-03-08 00:50:27 -0800

    javascript and images

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,15 +1,68 @@
+//VARIABLES================================================================================================
+
 var storedSelections = [],
+    wordBank = ['pikachu', 'mew', 'charmander', 'squirtle', 'bulbasaur', 'gengar', 'mewtwo', 'machamp', 'alakazam', 'ditto'],
     remainingGuesses,
     currentWord,
-    totalWins,
+    displayedWord,
+    totalWins = 0,
     keySelection;
 
+$(document).ready(function() {
+    startGame();
+});
+
+//FUNCTIONS================================================================================================
+
+function startGame() {
+    remainingGuesses = 12;
+    storedSelections = [];
+    currentWord = wordBank[Math.floor(Math.random() * wordBank.length)];
+    //console.log(currentWord);
+    displayedWord = '_'.repeat(currentWord.length);
+    $('.currentWordGuess').text(displayedWord.split('').join(' '));
+    $('.winCount').text(totalWins);
+    $('.totalGuessesLeft').text(remainingGuesses);
+    $('.pokemonImage').html('<img src="assets/images/pokeball.png" width=200px />');
+}
+
+function endGame() {
+    var playAgain = "Click here to play again";
+    $('.playerGuesses').text(playAgain);
+    $('.playerGuesses').on("click", function() {
+        startGame();
+        $('.playerGuesses').off("click");
+    }) 
+    $('.pokemonImage').html('<img src="assets/images/' + currentWord + '.png" width=200px />');
+}
+
+//JAVASCRIPT==============================================================================================
+
 document.onkeyup = function(event) {
-    keySelection = event.key.toLowerCase()
+    keySelection = event.key.toLowerCase();
     if (storedSelections.indexOf(keySelection) === -1) {
         storedSelections.push(keySelection);
-        remainingGuesses--;
+        $('.playerGuesses').text(storedSelections.join(' '));
+        if (currentWord.indexOf(keySelection) !== -1) {
+            for (var i = 0; i < currentWord.length; i++) {
+                if (currentWord.charAt(i) === keySelection) {
+                    displayedWord = displayedWord.substr(0, i) + keySelection + displayedWord.substr(i + 1);
+                }
+            }   
+        }
+        else {
+            remainingGuesses--;
+            $('.totalGuessesLeft').text(remainingGuesses)
+        }
     }
-
-    console.log(keySelection);
+    $('.currentWordGuess').text(displayedWord.split('').join(' '));
+    if (displayedWord === currentWord) {
+        $('.currentWordGuess').text('you win! the word was: ' + currentWord);
+        totalWins++;
+        endGame();
+    }
+    else if (remainingGuesses === 0) {
+        $('.currentWordGuess').text('too bad... the word was: ' + currentWord);
+        endGame();
+    }
 };
